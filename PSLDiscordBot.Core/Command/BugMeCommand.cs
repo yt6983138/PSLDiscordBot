f581e1076da4051ba48c249db5aeb316716ce8c9
yt6983138 <yt6983138@gmail.com>
2024-08-02 10:37:07 +0800

    Add bug me command

---
+++ PSLDiscordBot.Core/Command/BugMeCommand.cs
@@ -0,0 +1,24 @@
+﻿using Discord;
+using Discord.WebSocket;
+using PSLDiscordBot.Core.Command.Base;
+using PSLDiscordBot.Framework.CommandBase;
+
+namespace PSLDiscordBot.Core.Command.Template;
+
+[AddToGlobal]
+public class BugMeCommand : AdminCommandBase
+{
+	public override string Name => "bug-me";
+	public override string Description => "Can be used to test exception handling. [Admin command]";
+
+	public override SlashCommandBuilder CompleteBuilder =>
+		this.BasicBuilder;
+
+	public override async Task Execute(SocketSlashCommand arg, UserData? data, object executer)
+	{
+		await arg.ModifyOriginalResponseAsync(
+			x => x.Content = "Thrown exception.");
+
+		throw new NotImplementedException("Testing");
+	}
+}
