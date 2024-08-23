7902f8ed9a0529b7a548c80c46f442f08122eb59
yt6983138 <yt6983138@gmail.com>
2024-08-23 19:28:51 +0800

    Add BasicUserCommandBase for user commands.

---
+++ PSLDiscordBot.Framework/CommandBase/BasicUserCommandBase.cs
@@ -0,0 +1,19 @@
+﻿using Discord;
+using Discord.WebSocket;
+using PSLDiscordBot.Framework.DependencyInjection;
+
+namespace PSLDiscordBot.Framework.CommandBase;
+public abstract class BasicUserCommandBase : InjectableBase
+{
+	public abstract string Name { get; }
+	public virtual ApplicationIntegrationType[] IntegrationTypes =>
+		[ApplicationIntegrationType.UserInstall];
+
+	public virtual UserCommandBuilder BasicBuilder =>
+		new UserCommandBuilder()
+		.WithName(this.Name)
+		.WithIntegrationTypes(this.IntegrationTypes);
+	public virtual UserCommandBuilder CompleteBuilder => this.BasicBuilder;
+
+	public abstract Task Execute(SocketUserCommand arg, object executer);
+}
