51c96e4f4614aec178b468dc9d132e8a7b8800dd
yt6983138 <yt6983138@gmail.com>
2024-08-23 19:29:02 +0800

    Add MessageUserCommandBase for message commands.

---
+++ PSLDiscordBot.Framework/CommandBase/BasicMessageCommandBase.cs
@@ -0,0 +1,19 @@
+﻿using Discord;
+using Discord.WebSocket;
+using PSLDiscordBot.Framework.DependencyInjection;
+
+namespace PSLDiscordBot.Framework.CommandBase;
+public abstract class BasicMessageCommandBase : InjectableBase
+{
+	public abstract string Name { get; }
+	public virtual ApplicationIntegrationType[] IntegrationTypes =>
+		[ApplicationIntegrationType.UserInstall];
+
+	public virtual MessageCommandBuilder BasicBuilder =>
+		new MessageCommandBuilder()
+		.WithName(this.Name)
+		.WithIntegrationTypes(this.IntegrationTypes);
+	public virtual MessageCommandBuilder CompleteBuilder => this.BasicBuilder;
+
+	public abstract Task Execute(SocketMessageCommand arg, object executer);
+}
