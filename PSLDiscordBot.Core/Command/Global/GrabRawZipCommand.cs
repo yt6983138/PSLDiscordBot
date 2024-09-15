f979f099a2f059bed1bb2050547d1dd0c973fff7
yt6983138 <yt6983138@gmail.com>
2024-09-15 15:44:29 +0800

    Add grab raw zip command.

---
+++ PSLDiscordBot.Core/Command/Global/GrabRawZipCommand.cs
@@ -0,0 +1,61 @@
+﻿using Discord;
+using Discord.WebSocket;
+using PhigrosLibraryCSharp;
+using PSLDiscordBot.Core.Command.Global.Base;
+using PSLDiscordBot.Core.Services;
+using PSLDiscordBot.Core.UserDatas;
+using PSLDiscordBot.Framework;
+using PSLDiscordBot.Framework.CommandBase;
+
+namespace PSLDiscordBot.Core.Command.Global;
+
+[AddToGlobal]
+public class GrabRawZipCommand : AdminCommandBase
+{
+	public override string Name => "grab-raw-zip";
+	public override string Description => "Grab RAW zip by token. [Admin command]";
+
+	public override SlashCommandBuilder CompleteBuilder =>
+		this.BasicBuilder
+		.AddOption(
+			"token",
+			ApplicationCommandOptionType.String,
+			"Token.",
+			isRequired: true)
+		.AddOption(
+			"index",
+			ApplicationCommandOptionType.Integer,
+			"Index.",
+			isRequired: false);
+
+	public override async Task Callback(SocketSlashCommand arg, UserData? data, DataBaseService.DbDataRequester requester, object executer)
+	{
+		string token = arg.GetOption<string>("token");
+		int index = arg.GetIntegerOptionAsInt32OrDefault("index");
+
+		Save save;
+		MemoryStream newStream;
+		try
+		{
+			save = new(token);
+			byte[] d = await save.GetSaveRawZipAsync((await save.GetRawSaveFromCloudAsync()).GetParsedSaves()[index]);
+			newStream = new(d);
+		}
+		catch (Exception ex)
+		{
+			await arg.ModifyOriginalResponseAsync(
+				x => x.Content = ex.ToString());
+			return;
+		}
+
+		await arg.ModifyOriginalResponseAsync(
+			x =>
+			{
+				x.Content = "Grabbed!";
+				x.Attachments = new List<FileAttachment>()
+				{
+					new(newStream, "zip.zip")
+				};
+			});
+	}
+}
