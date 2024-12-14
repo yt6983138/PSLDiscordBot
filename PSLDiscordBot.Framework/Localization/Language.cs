9ceacc5ec4e6ef0ca3bba649a95bcfab819a3d27
yt6983138 <yt6983138@gmail.com>
2024-12-14 22:13:19 +0800

    Localization framework implementation

---
+++ PSLDiscordBot.Framework/Localization/Language.cs
@@ -0,0 +1,99 @@
+﻿namespace PSLDiscordBot.Framework.Localization;
+public enum Language
+{
+	[LanguageDetail("en-US", "English, US", "English, US")]
+	EnglishUS,
+
+	[LanguageDetail("id", "Indonesian", "Bahasa Indonesia")]
+	Idonesian,
+
+	[LanguageDetail("da", "Danish", "Dansk")]
+	Danish,
+
+	[LanguageDetail("de", "German", "Deutsch")]
+	German,
+
+	[LanguageDetail("en-GB", "English, UK", "English, UK")]
+	EnglishUK,
+
+	[LanguageDetail("es-ES", "Spanish", "Español")]
+	Spanish,
+
+	[LanguageDetail("es-419", "Spanish, LATAM", "Español, LATAM")]
+	SpanishLATAM,
+
+	[LanguageDetail("fr", "French", "Français")]
+	French,
+
+	[LanguageDetail("hr", "Croatian", "Hrvatski")]
+	Croatian,
+
+	[LanguageDetail("it", "Italian", "Italiano")]
+	Italian,
+
+	[LanguageDetail("lt", "Lithuanian", "Lietuviškai")]
+	Lithuanian,
+
+	[LanguageDetail("hu", "Hungarian", "Magyar")]
+	Hungarian,
+
+	[LanguageDetail("nl", "Dutch", "Nederlands")]
+	Dutch,
+
+	[LanguageDetail("no", "Norwegian", "Norsk")]
+	Norwegian,
+
+	[LanguageDetail("pl", "Polish", "Polski")]
+	Polish,
+
+	[LanguageDetail("pt-BR", "Portuguese, Brazilian", "Português do Brasil")]
+	PortugueseBrazilian,
+
+	[LanguageDetail("ro", "Romanian, Romania", "Română")]
+	RomainianRomania,
+
+	[LanguageDetail("fi", "Finnish", "Suomi")]
+	Finnish,
+
+	[LanguageDetail("sv-SE", "Swedish", "Svenska")]
+	Swedish,
+
+	[LanguageDetail("vi", "Vietnamese", "Tiếng Việt")]
+	Vietnamese,
+
+	[LanguageDetail("tr", "Turkish", "Türkçe")]
+	Turkish,
+
+	[LanguageDetail("cs", "Czech", "Čeština")]
+	Czech,
+
+	[LanguageDetail("el", "Greek", "Ελληνικά")]
+	Greek,
+
+	[LanguageDetail("bg", "Bulgarian", "български")]
+	Bulgarian,
+
+	[LanguageDetail("ru", "Russian", "Pусский")]
+	Russian,
+
+	[LanguageDetail("uk", "Ukrainian", "Українська")]
+	Ukrainian,
+
+	[LanguageDetail("hi", "Hindi", "हिन्दी")]
+	Hindi,
+
+	[LanguageDetail("th", "Thai", "ไทย")]
+	Thai,
+
+	[LanguageDetail("zh-CN", "Chinese, China", "中文")]
+	SimpifiedChinese,
+
+	[LanguageDetail("ja", "Japanese", "日本語")]
+	Japenese,
+
+	[LanguageDetail("zh-TW", "Chinese, Taiwan", "繁體中文")]
+	TraditionalChinese,
+
+	[LanguageDetail("ko", "Korean", "한국어")]
+	Korean
+}
---
+++ PSLDiscordBot.Framework/Localization/LocalizationNewtonsoftSerializer.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PSLDiscordBot.Framework.Localization;
+public class LocalizationNewtonsoftSerializer : JsonConverter<Localization>
+{
+	public override Localization? ReadJson(JsonReader reader, Type objectType, Localization? existingValue, bool hasExistingValue, JsonSerializer serializer)
+	{
+		if (reader.TokenType == JsonToken.Null) return null;
+
+		Localization ret = new();
+		ret.FallbackLanguages.Clear();
+		JToken obj = JObject.ReadFrom(reader);
+
+		string[] fallbackLangs = obj[nameof(Localization.FallbackLanguages)]!.ToObject<string[]>()!;
+		foreach (string item in fallbackLangs)
+		{
+			ret.FallbackLanguages.Add(LocalizationHelper.FromCode(item));
+		}
+
+		Dictionary<string, JObject> localizedStrings = obj[nameof(Localization.LocalizedStrings)]!.ToObject<Dictionary<string, JObject>>()!;
+		foreach (KeyValuePair<string, JObject> item in localizedStrings)
+		{
+			string key = item.Key;
+			LocalizedString str = item.Value.ToObject<LocalizedString>()!;
+			if (str.FallBackLanguages.SequenceEqual(ret.FallbackLanguages))
+				str.FallBackLanguages = ret.FallbackLanguages;
+			ret.Add(key, str);
+		}
+
+		return ret;
+	}
+
+	public override void WriteJson(JsonWriter writer, Localization? value, JsonSerializer serializer)
+	{
+		if (value is null)
+		{
+			writer.WriteNull();
+			return;
+		}
+
+		writer.WriteStartObject();
+		{
+			writer.WritePropertyName(nameof(Localization.FallbackLanguages));
+			writer.WriteStartArray();
+			foreach (Language item in value.FallbackLanguages)
+				writer.WriteValue(item.GetCode());
+			writer.WriteEndArray();
+
+			writer.WritePropertyName(nameof(Localization.LocalizedStrings));
+			{
+				serializer.Serialize(writer, value._localization);
+			}
+		}
+		writer.WriteEndObject();
+	}
+}
