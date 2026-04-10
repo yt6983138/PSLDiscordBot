915cfaf672cce0ccf9ab59c79aaaa90a537d2b58
yt6983138 <yt6983138@gmail.com>
2026-04-10 23:42:48 +0800

    Add a temp solution for fixing assembly loading (some 10.0.0 assemblies don't load properly for some reason)

---
+++ AssemblyFixer/AssemblyFixerPlugin.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using PSLDiscordBot.Framework;
+using System.Runtime.Loader;
+
+namespace AssemblyFixer;
+
+public class AssemblyFixerPlugin : IPlugin
+{
+	public string Name => "Assembly fixer";
+	public string Description => "Fix assembly loading";
+	public Version Version => new(1, 0, 0, 0);
+	public string Author => "yt6983138";
+	public int Priority => int.MinValue;
+	public bool CanBeDynamicallyLoaded => false;
+	public bool CanBeDynamicallyUnloaded => false;
+
+	static AssemblyFixerPlugin()
+	{
+		AssemblyLoadContext.Default.Resolving += (context, name) =>
+		{ // fuck you .net 10 assemblies, TODO: fix this piece of shit
+			if (name.Name == "System.IO.Pipelines")
+				return typeof(System.IO.Pipelines.Pipe).Assembly;
+			if (name.Name == "System.Text.Encodings.Web")
+				return typeof(System.Text.Encodings.Web.TextEncoder).Assembly;
+			if (name.Name == "System.Text.Json")
+				return typeof(System.Text.Json.JsonSerializer).Assembly;
+			return null;
+		};
+	}
+
+	public void Load(WebApplicationBuilder hostBuilder, bool isDynamicLoading)
+	{
+	}
+
+	public void Setup(IHost host)
+	{
+	}
+
+	public void Unload(IHost host, bool isDynamicUnloading)
+	{
+	}
+}
