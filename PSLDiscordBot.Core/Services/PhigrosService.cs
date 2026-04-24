0870641de7c007fa543acbd927f1e95a0688303a
yt6983138 <yt6983138@gmail.com>
2026-04-24 22:13:37 +0800

    Make info writable

--- PSLDiscordBot.Core/Services/PhigrosService.cs
+++ PSLDiscordBot.Core/Services/PhigrosService.cs
@@ -28,10 +28,10 @@ public class PhigrosService
 	private readonly Dictionary<ulong, CallbackLoginRequest> _callbackLoginRequests = [];
 	public IReadOnlyDictionary<ulong, CallbackLoginRequest> CallbackLoginRequests => this._callbackLoginRequests;
 
-	public NonMultiLanguageInfos NonMultiLanguageInfos { get; private set; } = new([], [], [], "0.0.0", 0, false);
+	public NonMultiLanguageInfos NonMultiLanguageInfos { get; set; } = new([], [], [], "0.0.0", 0, false);
 	// using Localization.Language intentionally since i already make sure enum names in PhiInfo.Core.Models.Language 
 	// are the same as Localization.Language
-	public Dictionary<Language, MultiLanguageInfos> MultiLanguageInfos { get; private set; } = [];
+	public Dictionary<Language, MultiLanguageInfos> MultiLanguageInfos { get; set; } = [];
 
 	public PhigrosService(IOptions<Config> config, ILogger<PhigrosService> logger, LocalizationService localization)
 	{
