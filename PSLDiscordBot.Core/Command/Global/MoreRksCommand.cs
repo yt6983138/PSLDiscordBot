f24149672e82c67b41436156d9feb70e8641aaea
static_void <83499886+yt6983138@users.noreply.github.com>
2026-05-02 17:26:19 +0800

    Add phi calculation support (#24)

--- PSLDiscordBot.Core/Command/Global/MoreRksCommand.cs
+++ PSLDiscordBot.Core/Command/Global/MoreRksCommand.cs
@@ -1,4 +1,5 @@
 ﻿using SmartFormat;
+using System.Collections.Immutable;
 using System.Text;
 
 namespace PSLDiscordBot.Core.Command.Global;
@@ -44,6 +45,7 @@ public class MoreRksCommand : CommandBase
 	public override async Task Callback(SocketSlashCommand arg, UserData data, DataBaseService.DbDataRequester requester, object executer)
 	{
 		int count = arg.GetIntegerOptionAsInt32OrDefault(this._localization[PSLNormalCommandKey.MoreRksOptionCountName], 10);
+		// this will later be set to value for single song, but now it is for entire calulated rks
 		double giveLeastRks = arg.GetOptionOrDefault(this._localization[PSLNormalCommandKey.MoreRksOptionGetAtLeastName], -1d);
 		int index = arg.GetIndexOption(this._localization);
 
@@ -56,20 +58,61 @@ public class MoreRksCommand : CommandBase
 		giveLeastRks = giveLeastRks < 0 ? Math.Round(rks, 2, MidpointRounding.ToEven) + 0.005d - rks : giveLeastRks;
 		giveLeastRks *= 30;
 
-		List<TargetRksScorePair> growableScores = save.GetCompleteScores(this._phigrosService.ChartConstantMap, this._phigrosService.NameMap)
+		List<CompleteScore> scoresWithoutPhi3 = scores.Skip(3).ToList();
+		List<TargetRksScorePair> growableScores = scoresWithoutPhi3
 			.Select(r => BuildPair(r, giveLeastRks, leastRksInBests))
 			.Where(r => r.AccIsInValidRange)
 			.ToList();
+		ImmutableArray<CompleteScore> originalPhi3NoPadding = scores.Take(3).Where(x => !x.Equals(default)).ToImmutableArray();
 
 		// growableScores.AddRange(save.Records
 		// 	.Select(r => BuildPair(r, giveLeastRks, leastRksInBests))
 		// 	.Where(x => x.AccIsInValidRange));
 
 		// phi scenario:
-		// 1. phi get into phi3
+		// 1. phi get into phi3 only (only in case for ppl who have like 0 phis or very easy phis)
 		// 2. phi get into phi3, also in b30
 		// 3. phi not get into phi 3, but in b30 (no need to process this since the first growable score would already take care of this)
-		// TODO: add phi calculation
+		float phi3MinCC = originalPhi3NoPadding.Select(x => (float?)x.ChartConstant).Min() ?? 0f;
+
+		// phi scenario 1
+		IEnumerable<TargetRksScorePair> phiScenario1Score = scoresWithoutPhi3
+			.Where(x => x.ChartConstant > phi3MinCC)
+			.Where(x => x.ChartConstant < leastRksInBests)
+			.Where(x => x.ChartConstant < phi3MinCC + giveLeastRks + (0.005d * 30))
+			.Select(x => new TargetRksScorePair(x.ChartConstant, 100d, x));
+		growableScores.AddRange(phiScenario1Score);
+
+		// phi scenario 2
+		IEnumerable<CompleteScore> scoresCanGetIntoScenario2 = scoresWithoutPhi3
+			.Where(x => x.ChartConstant > phi3MinCC)
+			.Where(x => x.ChartConstant >= leastRksInBests);
+
+		double originalPhi3Rks = originalPhi3NoPadding.Sum(x => x.Rks / 3d);
+		List<CompleteScore> phi3 = [];
+		foreach (CompleteScore item in scoresCanGetIntoScenario2)
+		{
+			phi3.Clear();
+			phi3.AddRange(originalPhi3NoPadding);
+			phi3.Add(item);
+			phi3.Sort((x, y) => x.ChartConstant.CompareTo(y.ChartConstant));
+			phi3.RemoveAt(0);
+			double extraPhi3Rks = phi3.Distinct().Sum(x => x.ChartConstant / 3d) - originalPhi3Rks;
+
+			double extraB27Rks = item.ChartConstant - Math.Max(leastRksInBests, item.Rks);
+			double total = extraB27Rks + extraPhi3Rks;
+			if (giveLeastRks < total && total < giveLeastRks + (0.005d * 30))
+				growableScores.Add(new TargetRksScorePair(item.ChartConstant, 100d, item));
+		}
+
+		// phi scenario 3
+		//IEnumerable<TargetRksScorePair> phiScenario3Score = scoresWithoutPhi3
+		//	.Where(x => x.ChartConstant <= phi3MinCC)
+		//	.Where(x => x.ChartConstant < rks + giveLeastRks + (0.005d * 30)) // to make sure not add too much scores that are not actually completable
+		//	.Where(x => x.ChartConstant > leastRksInBests + giveLeastRks) // to make sure not add scores that are already in b30
+		//	.Select(x => new TargetRksScorePair(x.ChartConstant, 100d, x));
+		//growableScores.AddRange(phiScenario3Score);
+
 		growableScores.Sort((x, y) => y.Score.Rks.CompareTo(x.Score.Rks));
 		int calculatedShowCounts = Math.Min(growableScores.Count, count);
 
