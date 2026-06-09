9c4e9b5b67246c3e318566afda8cc371bcc896e8
static_void <83499886+yt6983138@users.noreply.github.com>
2026-06-09 11:25:55 +0800

    Add AdminRoleIds property and helper method for admin role access checks

--- PSLDiscordBot.Core/Models/SongAlias/AliasTableAttribute.cs
+++ PSLDiscordBot.Core/Models/SongAlias/AliasTableAttribute.cs
@@ -5,6 +5,7 @@ public class AliasTableAttribute
 	public string? InheritsFrom { get; set; }
 	public List<string> OverriddenSongAliases { get; set; } = [];
 	public bool AllowInheritance { get; set; }
+	public List<ulong> AdminRoleIds { get; set; } = [];
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 	private AliasTableAttribute() { }
@@ -13,5 +14,10 @@ public class AliasTableAttribute
 	public AliasTableAttribute(string tableId)
 	{
 		this.TableId = tableId;
+	}
+
+	public bool IsUserInAdminRole(SocketGuildUser user)
+	{
+		return user.Roles.Any(x => this.AdminRoleIds.Contains(x.Id));
 	}
 }
